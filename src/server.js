9634821d2512f984a01e678987fca4e3a0d4f6db
Raihanmul <raihanmul10@gmail.com>
2025-10-23 13:56:33 +0700

    error middleware response

--- src/server.js
+++ src/server.js
@@ -2,17 +2,17 @@ import express from "express";
 import { testConnection } from "./config/db.js";
 import userRouter from "./routes/usersRoute.js";
 import productRouter from "./routes/productRoute.js";
-import cors from "cors";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 
 const app = express();
-
-app.use(cors());
 app.use(express.json());
 
 const port = 3000;
 
-app.use(userRouter);
-app.use(productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,5 @@
 import { pool } from "../config/db.js";
+import { ResponseError } from "../errors/responseError.js";
 
 export const getAllUser = async () => {
   const [users] = await pool.query(
@@ -13,6 +14,10 @@ export const getUserById = async (id) => {
     "SELECT id, fullname, username, email, role, address, phone_number, age FROM users WHERE id=?",
     [id]
   );
+
+  if (users.length === 0) {
+    throw new ResponseError(404, "User not found");
+  }
 
   return users[0];
 };
